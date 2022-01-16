b0d6b40a6c790d796d5da5e8d4905dcef42fdfcd
Adhartasdev <blecollen@icloud.com>
2022-01-16 16:41:32 +0000

    add categories dropdown

--- app.py
+++ app.py
@@ -91,7 +91,8 @@ def logout():
 
 @app.route("/add_definition")
 def add_definition():
-    return render_template("add_definition.html")
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    return render_template("add_definition.html", categories=categories)
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
